27e56c2db0c30886ffc7a01320788c7b7b92c453
fabian <bian251091@gmail.com>
2019-03-06 15:14:07 -0300

    routes for post, devise & rswag

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  mount Rswag::Api::Engine => '/api-docs'
+  mount Rswag::Ui::Engine => '/api-docs'
+  resources :posts
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
