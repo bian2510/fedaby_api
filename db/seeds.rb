30e9cd6dab98e1cfa32244d22c1dda51d13b096c
fabian <bian251091@gmail.com>
2019-03-10 20:10:49 -0300

    seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(email: 'bian2510@hotmail.com', password: '123456')
+Article.create(title: "test", content: 'test', user_id: 1)
