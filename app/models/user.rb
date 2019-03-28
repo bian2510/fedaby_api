d9c2b9ece49c985749f3e2880f85ee818c3f2561
fabian <bian251091@gmail.com>
2019-03-28 16:50:11 -0300

    relation user model with devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
+  # Include default devise modules.
+  devise :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :trackable, :validatable,
+          :omniauthable
+  include DeviseTokenAuth::Concerns::User
+  has_many :article
+  has_many :comment
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+
 end
