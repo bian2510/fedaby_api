8e41f2f64e9f7f051d51780e3b59834b7b437685
fabian <bian251091@gmail.com>
2019-03-28 16:55:15 -0300

    articles spec

--- spec/integration/articles_spec.rb
+++ spec/integration/articles_spec.rb
@@ -1,5 +1,4 @@
 require 'swagger_helper'
-require 'uri'
 describe 'Fedaby API' do
   path '/articles' do
     get 'Get all articles' do
@@ -31,13 +30,14 @@ describe 'Fedaby API' do
       response '201', 'Article created' do
         let(:article) {
           { title: 'test', content: 'test', image: 'test',
-            vote: 0, user_id: User.create(email: 'bian2510@hotmail.com', password: '123456').id }
+            user_id: User.create(email: "bian#{rand(100)}@hotmail.com",
+            password: '123456').id }
         }
         run_test!
       end
 
-      response '422', 'invalid request' do
-        let(:article) { { title: 2 } }
+      response '400', 'invalid request' do
+        let(:article) { { user_id: 'invalid' } }
         run_test!
       end
     end
@@ -71,6 +71,65 @@ describe 'Fedaby API' do
       end
 
       response '404', 'article not found' do
+        let(:id) { 'invalid' }
+        run_test!
+      end
+    end
+
+    put 'Edit an article' do
+      tags 'Articles'
+      consumes 'application/json', 'application/xml'
+      produces 'application/json'
+      parameter name: :id, in: :path, type: :string
+      parameter name: :article, in: :body, schema: {
+        type: :object,
+        properties: {
+          title: { type: :string },
+          content: { type: :string },
+          image: { type: :string },
+          vote: { type: :integer },
+          user_id: { type: :integer }
+        }
+      }
+      response '200', 'Edit succesfull' do
+        let(:id) { 
+            Article.create(title: 'test', content: 'test',
+            image: 'test', user_id:
+            User.create(email: 'bian2510@hotmail.com',
+            password: '123456').id).id
+          }
+        let(:article) { { title: 'string' } }
+        run_test!
+      end
+
+      response '400', "The article can't edit" do
+        let(:id) { 
+            Article.create(title: 'test', content: 'test',
+            image: 'test', user_id:
+            User.create(email: 'bian2510@hotmail.com',
+            password: '123456').id).id
+          }
+        let(:article) { { user_id: 'string' } }
+        run_test!
+      end
+    end
+
+    delete 'Delete an article' do
+      tags 'Articles'
+      consumes 'application/json', 'application/xml'
+      produces 'application/json'
+      parameter name: :id, in: :path, type: :string
+      response '204', 'Article has been delete' do
+        let(:id) { 
+          Article.create(title: 'test', content: 'test',
+          image: 'test', user_id:
+          User.create(email: 'bian2510@hotmail.com',
+          password: '123456').id).id
+        }
+        run_test!
+      end
+
+      response '404', 'Article not found' do
         let(:id) { 'invalid' }
         run_test!
       end
