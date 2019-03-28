af0e1c1950c38e3544f71b6efb8a00a9adc51aa1
fabian <bian251091@gmail.com>
2019-03-28 16:48:13 -0300

    add validation params & search_articles_by_title

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,17 +1,22 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :update, :destroy]
+  before_action :validates_params, only: [:create, :update, :destroy]
   #before_action :authenticate_user!, only: [:create]
 
   # GET /articles
   def index
     @articles = Article.all
-
-    render json: @articles
+    render json: @articles, each_serializer: ArticleSerializer
   end
 
   # GET /articles/1
   def show
     render json: @article
+  end
+
+  def search_articles_by_title
+    @articles = Article.where(title: params[:content])
+    render json: @articles
   end
 
   # POST /articles
@@ -40,13 +45,24 @@ class ArticlesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_article
-      @article = Article.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def article_params
-      params.require(:article).permit(:title, :content, :image, :vote, :user_id)
+  # Use callbacks to share common setup or constraints between actions.
+  def set_article
+    @article = Article.find(params[:id])
+  end
+  # Only allow a trusted parameter "white list" through.
+
+  def article_params
+    params.require(:article).permit(:title, :content, :image, :user_id)
+  end
+
+  def validates_params
+    param! :article, Hash do |b|
+      b.param! :title, String
+      b.param! :content, String
+      b.param! :image, String
+      b.param! :vote, Integer
+      b.param! :user_id, Integer
     end
+  end
 end
