a9fed21ada6faca7a62a2e0f473250433fb508e7
fabian <bian251091@gmail.com>
2019-03-28 16:53:14 -0300

    routes find_article, profile & articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  resources :articles
+  mount_devise_token_auth_for 'User', at: 'auth'
   mount Rswag::Api::Engine => '/api-docs'
   mount Rswag::Ui::Engine => '/api-docs'
-  devise_for :users
+  #devise_for :users
+  resources :profile, only: [:show]
+
+  get 'find_articles', to: :find_articles, controller: :articles
+  
+  resources :articles do
+    resources :comments
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
