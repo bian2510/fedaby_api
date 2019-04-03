5b7ddf003fc47718c3d655f49ec4dfd3d199b4fc
fabian <bian251091@gmail.com>
2019-04-03 12:45:09 -0300

    profile autocomplete

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,8 +2,16 @@ class ProfileController < ApplicationController
   before_action :set_profile, only: [:show]
   rescue_from ActiveRecord::RecordNotFound, with: :handle_record_not_found
   rescue_from RailsParam::Param::InvalidParameterError, with: :handle_invalid_parameter
+  
+  # Show a Profile
   def show
     render json: ProfileSerializer.new(@profile)
+  end
+
+  # Autocomplete for profile
+  def autocomplete_profile
+    @profiles = ::Autocomplete.new.autocomplete(params[:content], User, 'username')
+    return render json: @profiles if @profiles != []
   end
 
   private
