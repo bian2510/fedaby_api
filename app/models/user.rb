de7c495e435756a56ba6e1da659a03a17149c3bf
fabian <bian251091@gmail.com>
2019-04-12 12:35:48 -0300

    relation with follower model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,21 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable,
           :omniauthable
+
   include DeviseTokenAuth::Concerns::User
+  
   has_many :article
+
   has_many :comment
+
+  has_many :follows
+  
+  has_many :follower_relationships, foreign_key: :following_id, class_name: 'Follow'
+  has_many :followers, through: :follower_relationships, source: :follower
+
+  has_many :following_relationships, foreign_key: :user_id, class_name: 'Follow'
+  has_many :following, through: :following_relationships, source: :following
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-
 end
